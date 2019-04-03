f4c13585cc327f156a49fb1d38c39284db101c79
Igor Zahvatov <iz@nowmedia.ru>
2019-04-04 01:04:29 +0300

    partial

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=mysql;dbname=fitmost-test',
+            'username' => 'fitmost-test',
+            'password' => 'secret',
             'charset' => 'utf8',
         ],
         'mailer' => [
